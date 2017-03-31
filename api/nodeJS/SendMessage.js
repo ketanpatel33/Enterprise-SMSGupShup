3a68a9edb5807c8cd272fe30ad8a25a0199990b5
Ketan Patel <ketanpatel33@users.noreply.github.com>
2017-03-31 15:24:01 +0530

    Add files via upload

---
+++ api/nodeJS/SendMessage.js
@@ -0,0 +1,19 @@
+var request = require("request");
+
+var options = { method: 'POST',
+  url: 'http://enterprise.smsgupshup.com/GatewayAPI/rest',
+  form: 
+   { method: 'sendMessage',
+     send_to: '919820XXXXXX',
+     msg: 'This is sample test message from GupShup',
+     msg_type: 'TEXT',
+     userid: '2000XXXXXX',
+     auth_scheme: 'PLAIN',
+     password: 'XXXXX',
+     format: 'JSON' } };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
